2f5372bc0af8d1c115634e3274bc2faba75cff0e
Chris Madison <cmadison@us.ibm.com>
2015-10-07 15:05:44 -0400

    multipage support added

--- bin/x2sj.js
+++ bin/x2sj.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+'use strict' 
 
-var parseXlsx = require('excel');
-var cli = require('cli');
-var fs = require('fs');
+var cli = require('cli'),
+    xparser = require('../lib/xlsxParser'),
+    tojson = require('../lib/jsonLib');
 
 cli.parse({
 	file: [ 'i', 'An XLSX file to process', 'file', null],
@@ -16,37 +17,6 @@ cli.main(function(args,options) {
 		cli.getUsage();
 		exit();
 	}	
-
-	var buffer = '';
-
-	parseXlsx(options.file, function(err, data) {
-		if(err) throw err;
-
-		buffer +='{\n';	
-
-		var headers = data[0];
-		for(var row=1;row<data.length;row++) {
-			buffer+='  "'+data[row][0]+'" : {\n';
-			buffer+='    "doc" : {\n';
-			for (var col=1;col<headers.length;col++) {
-				buffer+='      "'+headers[col]+'" : "'+data[row][col]+'"' 
-				buffer+= ((col<headers.length-1)?',':'');
-				buffer+='\n';
-			}
-			buffer+='    }\n';
-			buffer+='  },\n'
-		}
-		buffer+='  "commit" : { }\n';
-		buffer+='}';
-		writeOutput(options.output,buffer);	
-	});
-
-	var writeOutput = function(file, data) {
-		fs.writeFile(file, data, function(error) {
-			if (error) {
-				console.error("write error:  " + error.message);
-			} 
-		});
-	};
-
+	tojson(xparser(options.file),options.output);
 });
+
